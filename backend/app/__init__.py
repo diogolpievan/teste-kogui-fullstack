afc3327fd3e003c1c3367c92d7383107553f5999
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-20 11:59:09 -0300

    fix: import problems

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from dotenv import load_dotenv
-from extensions import db, bcrypt, jwt
+from app.extensions import db, bcrypt, jwt
 import os
 
 
