29c561ee81594ce70ada42c8818f92ec8463f287
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-20 11:36:40 -0300

    feat: model usuario

---
+++ backend/app/models/usuario.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from extensions import db, bcrypt
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    IDUsuario = db.Column(db.Integer, primary_key=True)
+    Nome = db.Column(db.String(150), nullable=False)
+    Login = db.Column(db.String(100), unique=True, nullable=False)
+    Email = db.Column(db.String(120), unique=True, nullable=False)
+    Senha = db.Column(db.String(128), nullable=False)
+    DtInclusao = db.Column(db.DateTime, default=datetime.now())
+    DtAlteracao = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
+
+    def set_password(self, senha):
+        self.Senha = bcrypt.generate_password_hash(senha).decode('utf-8')
+
+    def check_password(self, senha):
+        return bcrypt.check_password_hash(self.Senha, senha)
+
+    def to_dict(self):
+        return {
+            "IDUsuario": self.IDUsuario,
+            "Nome": self.Nome,
+            "Login": self.Login,
+            "Email": self.Email,
+            "DtInclusao": self.DtInclusao.isoformat(),
+            "DtAlteracao": self.DtAlteracao.isoformat(),
+        }
