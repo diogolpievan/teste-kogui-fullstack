355929940bce10a09ff97fc872b83091c888316c
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-10-20 11:34:58 -0300

    feat(WIP): init app

---
+++ backend/app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from dotenv import load_dotenv
+from extensions import db, bcrypt, jwt
+import os
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    load_dotenv()
+    
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400 
+    
+    db.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+    CORS(app)
+    
+    from app.routes.usuarios import usuarios_bp
+    
+    app.register_blueprint(usuarios_bp, url_prefix='/api/usuarios')
+    
+    with app.app_context():
+        db.create_all()
+    
+    return app
